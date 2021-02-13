7ebb93b38f9539f2e036660e3dfa8b4486678b34
Konstantin Magin <qwertyuiop12345jomh@gmail.com>
2021-02-13 19:10:15 +0300

    dss

---
+++ src/com/company/MySet.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MySet implements Set {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    synchronized public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    synchronized public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    synchronized public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    synchronized public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    synchronized public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean removeAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] objects) {
+        return new Object[0];
+    }
+}
